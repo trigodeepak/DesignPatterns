6671569656b54e8a1bab40723519ae5f90430c18
Deepak Yadav <trigodeepak@gmail.com>
2024-08-11 09:28:59 +0530

    Update factoryPattern.java
    Updated to make it more aligned with the correct factory pattern.

--- factoryPattern.java
+++ factoryPattern.java
@@ -2,41 +2,76 @@
 
 // Question: We have to build a course website, an platorm could have multiple online course based on type(recorded, interactive) every course could have multiple modules.
 
-
-// factoryInterface.java
-// factory Interface
-interface courseFactory {
-  public Course createCourse(){}
+// Course class
+public abstract class Course{
   
+  String id,name;
+  String type;
+  List<Module> modules = new ArrayList<>();
+  int likes, peopleEnrolled;
+
+  public abstract void createCouse();
 }
 
-// Course class
-class Course{
-  class Module{
+public abstract class Module{
     string videoLink;
     List<String> assignments; 
     List<List<Question>> tests
-  }
-  String id,name;
-  String type;
-  List<Module> modules;
-  int likes, peopleEnrolled;
+}
+
+class introModule extends Module{
+}
+
+class summaryModule extends Module{
+}
+
+class videoModule extends Module{
+  
 }
 
 // creation of recordedCourse
-class recordedCourse implements coursefactory{
-  public Course createCourse(){
-    Course c = new Course();
-    c.type = "RECORDED";
-    return c;
+class recordedCourse extends Course{
+  @Override
+  public void createCourse(){
+    type = "RECORDED";
+    modules.add(new introModule());
+    modules.add(new summaryModule());
   }
 }
 
 // creation of interactiveCourse
-class interactiveCourse implements coursefactory{
-  public Course createCourse(){
-    Course c = new Course();
-    c.type = "INTERACTIVE";
-    return c;
+class interactiveCourse extends Course{
+  @Override
+  public void createCourse(){
+    type = "INTERACTIVE";
+    modules.add(new introModule());
+    modules.add(new videoModuel());
   }
 }
+
+
+// factoryInterface.java
+// factory Interface
+public class courseFactory {
+  public static Course getCourse(String type){
+    if(type == "RECORDED"){
+      return recordedCourse();
+    }else if(type == "INTERACTIVE"){
+      return interactiveCourse();
+    }
+    return null;
+  }
+}
+
+
+// Driver code: 
+public static void main(){
+  Course rec = courseFactory.getCourse("RECORDED");
+  Course interactive = courseFactory.getCourse("INTERACTIVE");
+  assert rec != null;
+  System.out.println(rec);
+  assert interactive != null;
+  System.out.prinln(interactive);
+  
+}
+
